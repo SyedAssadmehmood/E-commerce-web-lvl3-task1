78508aa1caf1ca49d1bee92ca0ac1f44e727938a
SyedAssadmehmood <116765426+SyedAssadmehmood@users.noreply.github.com>
2023-10-25 07:19:38 -0700

    Initial Commit !!!

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+          <div className="descriptionbox-description">
+          <p>eCommerce websites have become an integral part of any business’ success. The increase in online sales during the pandemic saw business owners moving from physical storefronts to digital solutions to sell online, helping to expand their reach and increase revenue.</p>
+          <p>      
+Whether you’re a beginner or already have a physical store, building an eCommerce site requires careful attention to design  sell online, helping to expand.</p>
+
+
+          </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
